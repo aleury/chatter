1c60d4e7a8b75a7b8fc2bcbe36259a3ca54b0748
Adam Eury <euryadam@gmail.com>
2022-09-10 23:24:04 -0400

    Refactor listing rooms feature.

--- lib/chatter/chat/room.ex
+++ lib/chatter/chat/room.ex
@@ -1,20 +1,9 @@
 defmodule Chatter.Chat.Room do
   use Ecto.Schema
-  import Ecto.Changeset
-
-  alias __MODULE__
 
   schema "chat_rooms" do
     field :name, :string
 
     timestamps()
-  end
-
-  @doc false
-  def changeset(%Room{} = room, attrs) do
-    room
-    |> cast(attrs, [:name])
-    |> validate_required([:name])
-    |> unique_constraint(:name)
   end
 end
--- test/chatter_web/features/user_visits_rooms_page_test.exs
+++ test/chatter_web/features/user_visits_rooms_page_test.exs
@@ -1,11 +1,20 @@
 defmodule ChatterWeb.UserVisitsRoomsPageTest do
   use ChatterWeb.FeatureCase, async: true
 
+  alias Chatter.Chat
+
   test "user visits rooms page to see a list of rooms", %{session: session} do
-    room = insert(:chat_room)
+    [room1, room2] = insert_pair(:chat_room)
 
     session
-    |> visit(Routes.chat_room_path(@endpoint, :index))
-    |> assert_has(Query.css(".room", text: room.name))
+    |> visit(rooms_index())
+    |> assert_has(room_name(room1))
+    |> assert_has(room_name(room2))
+  end
+
+  defp rooms_index, do: Routes.chat_room_path(@endpoint, :index)
+
+  defp room_name(%Chat.Room{} = room) do
+    Query.data("role", "room", text: room.name)
   end
 end
